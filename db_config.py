cf6aa2abf394fe7a1d1c24b08246a2467c29c3ea
jothyanandanch <143174534+jothyanandanch@users.noreply.github.com>
2025-07-29 10:42:04 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,13 +4,12 @@ from psycopg2 import Error
 def get_db_connection():
     try:
         conn=psycopg2.connect(
-            dbname='shop',
+            dbname='your-db',
             user='postgres',
-            password='nandu',
+            password='your-password',
             host='localhost',
             port='5432'
         )
         return conn
     except Error as e:
         print(f"Error Connecting to PostgreSQL databse: {e}")
-        
